a56cc22aed506f4e7ae397ecf568538f38028639
rklpoi5678 <rklpoi5678@naver.com>
2025-11-27 12:02:16 +0900

    Feat: 폰트및 기본폴더설정

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,24 +1,37 @@
 import "./globals.css";
 
-import { Geist, Geist_Mono } from "next/font/google";
+import { Noto_Sans_KR } from "next/font/google";
+import LocalFont from "next/font/local"
 
 import { rootMetadata } from "#/config/metadata";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const notoSansKr = Noto_Sans_KR({
+  variable: "--font-noto-sans-kr",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const baskinRobbins = LocalFont({
+  src: [
+    {
+      path: "../assets/fonts/BR_B.woff2",
+      weight: '100 900',
+      style: 'normal',
+    },
+    {
+      path: "../assets/fonts/BR_B.woff",
+      weight: '100 900',
+      style: 'normal'
+    }
+  ],
+  display: 'swap',
+  variable: '--font-baskin-robbins'
+})
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${notoSansKr.variable} ${baskinRobbins.variable} antialiased`}
       >
         {children}
       </body>
